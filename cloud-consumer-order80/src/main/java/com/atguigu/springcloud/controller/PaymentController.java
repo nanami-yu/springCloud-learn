512d0c9df7fee31dbe5a2233659cae51ab934129
fengyu <470927753@qq.com>
2021-04-05 00:22:26 +0800

    restTemplate

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,4 +37,23 @@ public class PaymentController {
     public CommonResult<Payment> getPaymentById(@PathVariable("id")int id){
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id,CommonResult.class);
     }
+
+    @GetMapping("/consumer/payment/createEntity")
+    public CommonResult<Payment> createEntity(Payment payment){
+        // return restTemplate.postForObject(PAYMENT_URL + "/payment/create",payment,CommonResult.class);
+        return restTemplate.postForEntity(PAYMENT_URL + "/payment/create",payment,CommonResult.class).getBody();
+    }
+
+    @GetMapping("/consumer/payment/getEntity/{id}")
+    public CommonResult<Payment> getPaymentByIdEntity(@PathVariable("id")int id){
+        // return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id,CommonResult.class);
+        ResponseEntity<CommonResult> common =  restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id,CommonResult.class);
+
+        if(common.getStatusCode().is2xxSuccessful()){
+            return common.getBody();
+        }else{
+            return new CommonResult<>(444,"操作失败");
+        }
+    }
+
 }
--- cloud-consumerconsul-order80/src/main/java/com/atguige/springcloud/OrderConsulMain80.java
+++ cloud-consumerconsul-order80/src/main/java/com/atguige/springcloud/OrderConsulMain80.java
@@ -12,9 +12,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-public class OrderMain80 {
+public class OrderConsulMain80 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(OrderConsulMain80.class,args);
     }
 }
 
