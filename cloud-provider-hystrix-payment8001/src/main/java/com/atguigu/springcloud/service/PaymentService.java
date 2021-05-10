c8cb5eae6a3f9a3b2bf9b59103763c24720b7e48
赵刚 <zhaogang@cosco-shipyard.com>
2021-05-10 17:19:45 +0800

    *)HystrixCommand

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -1,8 +1,10 @@
 package com.atguigu.springcloud.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.concurrent.TimeUnit;
 
@@ -52,7 +54,42 @@ public class PaymentService {
 //        return "线程池： " + Thread.currentThread().getName() + "  paymentInfo_timeout,id:" + id + " 结果为" + a;
     }
 
+    /**
+     * @Description:访问超时 降级方法
+     *
+     * @param id
+     * @author fengyu
+     * @date 2021/5/10 16:06
+     * @return java.lang.String
+     */
     public String timeOutHandler(Integer id){
         return "线程池： " + Thread.currentThread().getName() + "  paymentInfo_timeout,id:" + id + " 服务超时";
+    }
+
+    //======================================== 服务熔断 =======================================
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback",commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"), // 是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"), // 请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"), // 时间窗口期
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60"), // 失败率
+    })
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+        if(id < 0){
+            throw new RuntimeException("id 不能为负数");
+        }
+        String serialNumber = IdUtil.simpleUUID();
+        return Thread.currentThread().getName() + "\t" + "调用成功,流水号:" + serialNumber;
+    }
+
+    /**
+     * @Description:访问超时 降级方法
+     *
+     * @param id
+     * @author fengyu
+     * @date 2021/5/10 16:06
+     * @return java.lang.String
+     */
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id){
+        return "触发降级 id:" + id;
     }
 }
